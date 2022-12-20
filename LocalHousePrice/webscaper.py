b1aa417f3925277ca1bb87651626cfc029d29057
Bananenn <af223krr@student.lnu.se>
2022-12-20 17:04:51 +0100

    progress on fitting image with coordinates

--- LocalHousePrice/webscaper.py
+++ LocalHousePrice/webscaper.py
@@ -1,8 +1,11 @@
 import requests
 import json
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
-import string
+
+import matplotlib.cbook as cbook
+import matplotlib.image as image
 
 import subprocess
 
@@ -39,31 +42,73 @@ def scrapeBooli(nPages):
 
 #scrapeBooli(10)
 
+# explicit function to normalize array
+def normalize(arr, t_min, t_max):
+    norm_arr = []
+    diff = t_max - t_min
+    diff_arr = max(arr) - min(arr)   
+    for i in arr:
+        temp = (((i - min(arr))*diff)/diff_arr) + t_min
+        norm_arr.append(temp)
+    return norm_arr
+
+
 def printCoordinatesOnmap():
   with open('tempJSON2.txt') as f:
       data = f.read()
+      
 
   json_object = json.loads(data)
 
   lat_long = []
-  for obj in json_object[:2]:
+  for obj in json_object[1:]:
     lat_long.append([obj['latitude'],obj['longitude']])
-    print(obj['streetAddress'])
-
+    print(obj["streetAddress"])
 
   lat_long = np.array(lat_long)
-  fig, ax = plt.subplots()
-  img = plt.imread("LocalHousePrice\\vxoimg.PNG")
-  ax.imshow(img, extent=[56.833862, 56.926051 ,14.726890, 14.865267])
-  print("X latitude ", lat_long[:,0], " Y longitude ", lat_long[:,1])
-  ax.scatter(x=lat_long[:,0], y=lat_long[:,1],marker=".")
-  ax.scatter(56.880544, 14.833087, marker="x")
-  print(len(lat_long))
-
+  lat_long
+  print("X latitude ", lat_long[:,0],".",lat_long[:,1])
+  datafile = cbook.get_sample_data("C:\LocalHousePrice\LocalHousePrice\\vxoimgFlip.png", asfileobj=False)
+  
+  im = image.imread(datafile)
+#                                           (left, right, bottom, top)
+  myaximage = plt.imshow(im, alpha=0.5, zorder=-1, origin='lower') 
+  plt.scatter(x=normalize(lat_long[:,1],0, 570), y=normalize(lat_long[:,0], 0, 704),marker=".")
+  #ax = plt.gca()
+  #ax.set_aspect('equal' , adjustable='box')
+  plt.grid()
   plt.show()
+  """
+  ax.scatter(x=lat_long[:,0], y=lat_long[:,1])
+  ax.scatter(56.880457, 14.833258,marker="x")
+  ax.scatter(56.850406, 14.843866,marker="x")
+  ax.scatter(56.889891, 14.753340,marker="x")
+  ax.scatter(56.855765, 14.778129,marker="x")
+  ax.scatter(56.901417, 14.825091,marker="x")
+  ax.plot([56.880588,56.9043], [14.833065,14.7973] )
+  ax.plot([56.850497,56.9113], [14.843374, 14.7532] )
+  ax.plot([56.901380, 56.8984], [14.825090, 14.8284] )
+  ax.plot([56.855248, 56.8675], [14.778068, 14.7600] )
+  ax.plot([56.889892, 56.8508], [14.753486, 14.8117] )
+  ax.imshow(img, extent=[56.926051, 56.833862,14.865267, 14.726890],origin="upper") 
+  """
 
 printCoordinatesOnmap()
 
 
-#print("Nice json ", bauty_json)
+# My            Real
+# X  bottom left = bottom left
+# -  Bottom right  = Top left
+# -  Top left = Bottom Right
+# X  Top right = Top right 
 
+# My            Real
+# X  bottom left = Bottom left
+# -  Bottom right  = 
+# -  Top left = X
+# X  Top right = Top Right 
+
+# 56.8339,14.7277 Bottom left = True
+# 56.9256,14.7277 Bottom right = top left
+# 56.8339,14.8644 TOP left = Bottom right
+# 56.9256,14.8644 Top right = top right
